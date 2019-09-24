05de9095c3e45ab3d85fd1faecbb359d96ae0db8
Arthurlemus <arthurlemus@hotmail.com>
2019-09-24 18:02:16 -0500

    Empezando SpotifyApp
    Se empieza con el nuevo proyecto

--- AngularApp/03-pipes/src/app/app.component.ts
+++ AngularApp/03-pipes/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +9,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pipes';
   nombre = 'Irving';
-  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  nombre2 = 'irving arturo leMUs alEMan';
+  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   PI = Math.PI;
   a = 0.234;
   salario = 1234.5;
+
+  heroe = {
+    nombre: 'logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: '19'
+    }
+  };
+
+  valorDePromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Llegaron los Datos'), 3500);
+  });
+
+  fecha = new Date();
+
+  video = '4KrJV7UwWJU';
+
+  activar = true;
+
+  pass = 'password';
+
 }
