9bdc7ffbd513155e281cf1e6fca6e21707c1011e
ArthurLemus <demoler_3@hotmail.com>
2019-10-13 22:07:47 -0500

    Formulario V3

--- AngularApp/08-formularios/src/app/components/data/data.component.ts
+++ AngularApp/08-formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -43,21 +44,24 @@ export class DataComponent implements OnInit {
                                       Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
                                 ]),
       pasatiempos: new FormArray([
-        new FormControl('', Validators.required)
+        new FormControl('Correr', Validators.required)
       ]),
-      username: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required, this.existeUsuario),
       password1: new FormControl('', Validators.required),
       password2: new FormControl()
     });
 
     this.forma.get('password2').setValidators([Validators.required, this.noIgual.bind(this.forma)]);
     // this.forma.setValue(this.usuario);
+
+    this.forma.get('username').valueChanges.subscribe(datos => {
+      console.log(datos);
+    });
   }
 
   ngOnInit() {
   }
 
-  
   noIgual(control: FormControl): any {
     console.log(control);
     if (control.value !== this.get('password1').value) {
@@ -66,8 +70,20 @@ export class DataComponent implements OnInit {
     return null;
   }
 
-  existeUsuario(control: FormControl) {
-    // PAra verificar que el usuario existe
+  existeUsuario(control: FormControl): Promise<any>|Observable<any> {
+    let promesa = new Promise((resolve, reject) => {
+       setTimeout(() => {
+         if (control.value === 'strider') {
+           resolve({existe: true});
+         } else {
+          resolve(null);
+         }
+       }, 3000);
+    });
+    // console.log('promesaaaaa');
+    // console.log(promesa);
+    return promesa;
+
   }
 
 
