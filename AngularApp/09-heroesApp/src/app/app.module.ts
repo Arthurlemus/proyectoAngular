382406576573cc9a4e6971baf3a118bcc75c466c
Arthurlemus <arthurlemus@hotmail.com>
2019-11-03 23:08:41 -0600

    Nuevos Folder
    Nuevas carpetas despues del formateo

---
+++ AngularApp/09-heroesApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroeComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
