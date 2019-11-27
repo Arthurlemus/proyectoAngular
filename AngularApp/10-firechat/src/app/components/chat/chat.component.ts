a2bb4b959804a779803ae88f3e01f6b4cac06af9
Arthurlemus <arthurlemus@hotmail.com>
2019-11-27 02:14:39 -0600

    Chat 1.0
    Se agregaron nuevas cosas

--- AngularApp/10-firechat/src/app/components/chat/chat.component.ts
+++ AngularApp/10-firechat/src/app/components/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+
 
 @Component({
   selector: 'app-chat',
@@ -8,13 +10,29 @@ import { Component, OnInit } from '@angular/core';
 export class ChatComponent implements OnInit {
 
   mensaje: string;
-  constructor() { }
+  elemento: any; // PAra el scroll
+
+  constructor( public chatService: ChatService) {
+
+    this.chatService.cargarMensajes().subscribe(() => {
+         setTimeout(() => {
+           this.elemento.scrollTop = this.elemento.scrollHeight; // Mantiene el scroll abajo
+         }, 20);
+    });
+
+  }
 
   ngOnInit() {
+    this.elemento = document.getElementById('app-mensajes'); // Toma el dato del Elemento con ese ID
   }
 
   enviar_mensaje() {
     console.log(this.mensaje);
+    if (this.mensaje.length === 0) {
+      return;
+    }
+    this.chatService.agregarMensaje(this.mensaje).then(() => this.mensaje = '')
+                                      .catch((err) => console.error('Error al guardar: ', err));
   }
 
 }
