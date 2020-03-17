33ad70ffa58cc5b4e0512daff5eb600e4bfb87a6
Arthur Lemus <arthurlemus@hotmail.com>
2020-03-17 14:07:33 -0600

    prueba
    s

--- RepasoAngular/typescript/app.js
+++ RepasoAngular/typescript/app.js
@@ -1,14 +1,23 @@
-"use strict";
 (function () {
-    function saludar(nombre) {
-        console.table('Welcome ' + nombre); // Hola Logan
-    }
-    var wolverine = {
-        nombre: 'Logan'
+    //     function saludar(nombre: string) {
+    //     console.table('Welcome ' + nombre); // Hola Logan
+    // }
+    // const wolverine = {
+    //     nombre: 'Logan'
+    // };
+    // saludar(wolverine.nombre);
+    // const sumarN = (a: number, b: number)=>{
+    //     return a + b;
+    // }
+    // console.log('La suma es:',sumarN(5,10));
+    var hulk = {
+        nombre: 'Brus Banner',
+        smash: function () {
+            var _this = this;
+            setTimeout(function () {
+                console.log(_this.nombre + " Se a Convertido");
+            }, 1000);
+        }
     };
-    saludar(wolverine.nombre);
-    var sumarN = function (a, b) {
-        return a + b;
-    };
-    console.log('La suma es:', sumarN(5, 10));
+    hulk.smash();
 })();
