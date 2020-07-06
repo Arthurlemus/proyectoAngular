14afbd74c6d09d8557209aa8206a4dd0f2e243c1
ArthurLemus <arthurlemus@hotmail.com>
2020-07-06 17:58:44 -0500

    Update app.ts

--- AngularApp/app.ts
+++ AngularApp/app.ts
@@ -4,19 +4,16 @@
   const nombre = 'Ricardo Tapia';
   const edad = 23;
 
-  let PERSONAJE = {
-    nombre: nombre,
-    edad: edad
-  };
+  let PERSONAJE = { nombre, edad };
   console.log(PERSONAJE);
 
   // Cree una interfaz que sirva para validar el siguiente objeto
-  interface DC {
+  interface Batman {
     nombre: string;
-    artesMarciales: Array<string>;
+    artesMarciales: string[];
   };
 
-  let batman: DC = {
+  let batman: Batman = {
     nombre: 'Bruno Díaz',
     artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
   }
@@ -27,15 +24,15 @@
     return (a + b) * 2
   }
 
-  let resultadoDobleF = ( a , b ) => (a+b)*2;
+  let resultadoDobleF = ( a: number , b: number ) => (a+b)*2;
   
   console.log(resultadoDobleF(5,10));
   // Función con parametros obligatorios, opcionales y por defecto
   // donde NOMBRE = obligatorio
   //       PODER  = opcional
   //       ARMA   = por defecto = 'arco'
-  function getAvenger( nombre, poder?, arma = 'arco' ){
-    var mensaje;
+  function getAvenger( nombre: string, poder?:string, arma: string = 'arco' ){
+    let mensaje;
     if( poder ){
       mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
     }else{
@@ -53,17 +50,15 @@
   // También un método que calcule el área  =  base * altura,
   // ese método debe de retornar un numero.
 
-class rectangulo {
-  constructor(){
-    let base = 0;
-    let altura = 0;
+class Rectangulo {
+  constructor(public base: number, public altura: number ){
   };
 
-  area(base, altura): number{
-    return (base * altura);
+  calcularArea(){
+    return (this.base * this.altura);
   }
+
 };
 
-rectangulo.area();
 
 })();
