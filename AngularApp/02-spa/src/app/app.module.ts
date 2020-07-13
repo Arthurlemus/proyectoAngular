461ea4451b89a1737b834586f98fc76c4b21f6a4
ArthurLemus <arthurlemus@hotmail.com>
2020-07-13 18:58:15 -0500

    Terminando SPA
    Se esta terminando la aplicacion del Spa

--- AngularApp/02-spa/src/app/app.module.ts
+++ AngularApp/02-spa/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
+import { AppRoutingModule } from './app.routes';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { FormsModule } from '@angular/forms';
+import { FiltroComponent } from './components/filtro/filtro.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    FiltroComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularApp/02-spa/src/app/components/shared/navbar/navbar.component.ts
+++ AngularApp/02-spa/src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  buscarHeroe(filtro: string) {
+    if (filtro.length > 0){
+      this.router.navigate(['/filtro', filtro]);
+      return;
+    }
+    this.router.navigate(['/heroes']);
   }
 
 }
