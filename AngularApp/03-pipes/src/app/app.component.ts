2b4ecc4e4e5b7b0e11edd2f83022c83fb36a4d51
ArthurLemus <arthurlemus@hotmail.com>
2020-07-16 18:19:57 -0500

    pipes

--- AngularApp/03-pipes/src/app/app.component.ts
+++ AngularApp/03-pipes/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'Capitan America';
+  nombre = 'Irving Lemus';
+  arreglo = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 }
