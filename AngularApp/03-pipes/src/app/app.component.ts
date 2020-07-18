8976bd3d541dc2d1e8035ac80d48301a09ec93a5
ArthurLemus <arthurlemus@hotmail.com>
2020-07-18 14:09:08 -0500

    Pipe Currency
    se termino de revisar

--- AngularApp/03-pipes/src/app/app.component.ts
+++ AngularApp/03-pipes/src/app/app.component.ts
@@ -9,4 +9,6 @@ export class AppComponent {
   nombre = 'Irving Lemus';
   arreglo: string[] = ['1', '2', '3', '4', '5', '6' , '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
   PI: number = Math.PI;
+  porcentaje: number = 0.256;
+  moneda = 1234.5;
 }
