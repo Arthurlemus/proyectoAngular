15e9c5e1941234d14a8f9553157c8f93881a6ed3
ArthurLemus <arthurlemus@hotmail.com>
2020-07-21 01:01:38 -0500

    Avances

--- AngularApp/04-spotiapp/src/app/app.routes.ts
+++ AngularApp/04-spotiapp/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
 
 const routes: Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'search', component: SearchComponent},
+    {path: 'artista', component: ArtistaComponent},
     {path: '', pathMatch: 'full', redirectTo: 'home'},
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
