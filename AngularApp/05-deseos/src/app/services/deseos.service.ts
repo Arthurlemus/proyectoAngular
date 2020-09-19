2ecb03034393ae5c0832781332c7dda56d6b652b
ArthurLemus <arthurlemus@hotmail.com>
2020-09-19 14:15:33 -0500

    nuevo

--- AngularApp/05-deseos/src/app/services/deseos.service.ts
+++ AngularApp/05-deseos/src/app/services/deseos.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeseosService {
-  lista: any[] = [];
-  constructor() { }
+  public listas: Lista[] = [];
+
+  constructor() {
+    const lista1 = new Lista('Recolectar piedras del infinito');
+    const lista2 = new Lista('Heroes a desaparecer');
+    const lista3 = new Lista('Lista Personal');
+    this.listas.push(lista1, lista2, lista3);
+  }
 }
