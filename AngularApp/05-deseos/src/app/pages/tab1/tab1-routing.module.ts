d821079d5187e5b96eb74df05427342a4715a5ca
ArthurLemus <arthurlemus@hotmail.com>
2020-09-21 01:36:14 -0500

    Avances
    Se inicia con la pantalla de agregar

--- AngularApp/05-deseos/src/app/pages/tab1/tab1-routing.module.ts
+++ AngularApp/05-deseos/src/app/pages/tab1/tab1-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
+import { AgregarPageModule } from '../agregar/agregar.module';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
+  },
+  {
+    path: 'agregar',
+    loadChildren: () => import('../agregar/agregar.module').then(m => m.AgregarPageModule)
   }
 ];
 
